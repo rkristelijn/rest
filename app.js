dad2cb79fd04beb617d730284c35135d937c4a2b
Remi Kristelijn <rkristelijn@gmail.com>
2018-01-22 09:43:25 +0000

    fixed a model

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var express = require('express');
+var express = require('express'),
+  mongoose = require('mongoose');
 
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+var Book = require('./books/book-model');
 var app = express();
-
 var port = process.env.PORT || 3100;
 
-app.get('/', function(req, res) {
+var bookRouter = express.Router();
+bookRouter.route('/books')
+  .get(function (req, res) {
+    var responseJson = { hello: "This is my api" };
+    res.json(responseJson);
+  });
+
+app.use('/api', bookRouter);
+app.get('/', function (req, res) {
   res.send('Welcome to my API!');
 });
 
-app.listen(port, function(){
+app.listen(port, function () {
   console.log('Running on PORT: ' + port);
 });
