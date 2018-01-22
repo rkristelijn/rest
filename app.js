a5b4bd49b2e684a3af8ef8b5d2363f59b458dc04
Remi Kristelijn <rkristelijn@gmail.com>
2018-01-22 09:10:44 +0000

    fixed chapter 1 with gulp

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3100;
+
+app.get('/', function(req, res) {
+  res.send('Welcome to my API!');
+});
+
+app.listen(port, function(){
+  console.log('Running on PORT: ' + port);
+});
