5d4c51f327029d88a3cc86c475204a29db833d54
Remi Kristelijn <rkristelijn@gmail.com>
2018-01-23 11:12:33 +0000

    fixed working post test

--- books/book-controller.js
+++ books/book-controller.js
@@ -1,9 +1,14 @@
 let bookController = function (Book) {
   let post = function (req, res) {
     let book = new Book(req.body);
-    book.save();
-    res.status(201);
-    res.send(book);
+    if (!req.body.title) {
+      res.status(400);
+      res.send('Title is required');
+    } else {
+      book.save();
+      res.status(201);
+      res.send(book);
+    }
   };
   let get = function (req, res) {
     let query = {};
