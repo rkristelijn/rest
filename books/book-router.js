540d17a71242b922b051da3f0429881b97fe63f3
Remi Kristelijn <rkristelijn@gmail.com>
2018-01-23 07:37:29 +0000

    fixed routes

---
+++ books/book-router.js
@@ -0,0 +1,31 @@
+let express = require('express'),
+ Book = require('./book-model');
+
+let routes = function () {
+  let bookRouter = express.Router();
+  bookRouter.route('/books')
+    .post(function (req, res) {
+      let book = new Book(req.body);
+      book.save();
+      res.status(201).send(book);
+    })
+    .get(function (req, res) {
+      let query = {};
+      if (req.query.genre) query.genre = req.query.genre;
+      Book.find(query, function (err, books) {
+        if (err) res.status(500).send(err);
+        else res.json(books);
+      });
+    });
+  bookRouter.route('/books/:bookId')
+    .get(function (req, res) {
+      Book.findById(req.params.bookId, function (err, book) {
+        if (err) res.status(500).send(err);
+        else res.json(book);
+      });
+    });
+
+    return bookRouter;
+};
+
+module.exports = routes;
